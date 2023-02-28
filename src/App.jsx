bb4221d12abc57882783b3432b6a28a75e8e149e
Nikhil Anand <102014379+Nikhil-gna@users.noreply.github.com>
2023-02-28 22:58:38 +0530

    Header and Footer

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Note from "./Note.jsx";
+import Header from "./Header";
+import Footer from "./Footer";
+import React from "react";
+
+function App() {
+    return (
+        <div>
+        <Header />
+        <Footer />
+        <Note />
+        
+        </div>
+    );
+    }
+
+    export default App;
---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Footer(){
+    return(<footer>
+        <p>CopyRight &copy; {new Date().getFullYear()}</p>
+    </footer>);
+
+}
+export default Footer;
