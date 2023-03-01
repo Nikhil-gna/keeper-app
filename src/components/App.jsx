e643b62c54fd29c96ec97b868a0b8cbb07d1b393
Nikhil Anand <102014379+Nikhil-gna@users.noreply.github.com>
2023-03-01 22:12:57 +0530

    updated multiple Notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,24 @@ import Note from "./Note.jsx";
 import Header from "./Header";
 import Footer from "./Footer";
 import React from "react";
+import notes from "../notes"
+
 
 function App() {
     return (
         <div>
         <Header />
+
+        {notes.map((data) => (
+          <Note
+            key={data.key}
+            title={data.title}
+            content={data.content}
+            />
+         ))}
+
+
         <Footer />
-        <Note />
-        
         </div>
     );
     }
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
 
-function Note(){
+function Note(props){
     return(<div className="note">
-    <h1>This is the note title</h1>
-    <p>paragraph</p>
+    <h1>{props.title}</h1>
+    <p>{props.content}</p>
 </div>
 );
-
+ 
 }
 
 export default Note;
